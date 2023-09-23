8f6ab03fa3d13dee6dd59093f1101600810d6e1b
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2023-09-23 20:27:47 +0300

    Handling Errors

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -12,15 +12,15 @@ interface Todo {
 const TodoList = () => {
   const fetchTodos = () =>
     axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .get<Todo[]>("https://xjsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
 
-  const { data: todos } = useQuery({
+  const { data: todos, error } = useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
   });
 
-  // if (error) return <p>{error}</p>;
+  if (error) return <p>{error.message}</p>;
 
   return (
     <ul className="list-group">
