70bdbe4832c96833a47cda6e237b16ca295be3fc
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2023-11-12 20:39:35 +0200

    Auth Reducer - (Exercise- Working with Reducers)

--- src/state-management/LoginStatus.tsx
+++ src/state-management/LoginStatus.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useReducer } from "react";
+import authReducer from "./reducers/authReducer";
 
 const LoginStatus = () => {
-  const [user, setUser] = useState('');
+  const [user, dispatch] = useReducer(authReducer, "");
 
   if (user)
     return (
       <>
         <div>
           <span className="mx-2">{user}</span>
-          <a onClick={() => setUser('')} href="#">
+          <a onClick={() => dispatch({ type: "LOGOUT" })} href="#">
             Logout
           </a>
         </div>
@@ -16,7 +17,7 @@ const LoginStatus = () => {
     );
   return (
     <div>
-      <a onClick={() => setUser('mosh.hamedani')} href="#">
+      <a onClick={() => dispatch({ type: "LOGIN", username: "mosh" })} href="#">
         Login
       </a>
     </div>
---
+++ src/state-management/reducers/authReducer.ts
@@ -0,0 +1,18 @@
+interface LoginAction {
+  type: "LOGIN";
+  username: string;
+}
+
+interface LogoutAction {
+  type: "LOGOUT";
+}
+
+type AuthAction = LoginAction | LogoutAction;
+
+const authReducer = (state: string, action: AuthAction): string => {
+  if (action.type === "LOGIN") return action.username;
+  if (action.type === "LOGOUT") return "";
+  return state;
+};
+
+export default authReducer;
