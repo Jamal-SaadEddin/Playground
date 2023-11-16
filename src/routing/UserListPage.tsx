05b96db68b307de326d4fe983627b792480cfff5
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2023-11-16 11:02:06 +0200

    Passing Data with Route Parameters

--- src/routing/UserListPage.tsx
+++ src/routing/UserListPage.tsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 const UserListPage = () => {
   const users = [
-    { id: 1, name: 'Mosh' },
-    { id: 2, name: 'John' },
-    { id: 3, name: 'Alice' },
+    { id: 1, name: "Mosh" },
+    { id: 2, name: "John" },
+    { id: 3, name: "Alice" },
   ];
   return (
     <ul className="list-group">
       {users.map((user) => (
         <li className="list-group-item" key={user.id}>
-          <a href="#">{user.name}</a>
+          <Link to={`/users/${user.id}`}>{user.name}</Link>
         </li>
       ))}
     </ul>
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
+import UserDetailPage from "./UserDetailPage";
 import UserListPage from "./UserListPage";
-import ContactPage from "./ContactPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/users", element: <UserListPage /> },
-  { path: "/contact", element: <ContactPage /> },
+  { path: "/users/:id", element: <UserDetailPage /> },
 ]);
 
 export default router;
