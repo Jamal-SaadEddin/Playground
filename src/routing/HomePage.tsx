c7c75f16c2b32635ea8a15774b790817fd815f27
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2023-11-17 15:35:19 +0200

    Handling Errors, Invalid Routes

--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  throw new Error();
+
   return (
     <>
       <p>
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,11 +3,13 @@ import HomePage from "./HomePage";
 import Layout from "./Layout";
 import UserDetail from "./UserDetail";
 import UsersPage from "./UsersPage";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
